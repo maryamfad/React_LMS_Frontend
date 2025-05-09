487eae0080335025784dde792c99f79d3e7d635f
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-09 11:33:39 -0400

    integrate clerk package for authentication

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 const Navbar = () => {
 	const isCourseListPage = location.pathname.includes("/course-list");
+
+	const { openSignIn } = useClerk();
+	const { user } = useUser();
 	return (
 		<div
 			className={`flex gap-5 items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 py-4 border-b ${
@@ -32,33 +36,15 @@ const Navbar = () => {
 				</Link>
 			</div>
 
-			<button className="hidden md:inline-block bg-white text-blue-600 font-semibold px-5 py-2 rounded-full shadow hover:bg-blue-100 transition duration-300">
+			<button
+				onClick={() => openSignIn()}
+				className="hidden md:inline-block bg-white text-blue-600 font-semibold px-5 py-2 rounded-full shadow hover:bg-blue-100 transition duration-300"
+			>
 				Create Account
 			</button>
-			{/* For phone Screens*/}
-			{/* <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
-				<div>
-					<Link
-						to="/become-educator"
-						className="hover:underline transition duration-200"
-					>
-						Become Educator
-					</Link>
-					<Link
-						to="/my-enrollments"
-						className="hover:underline transition duration-200"
-					>
-						My Enrollments
-					</Link>
-				</div>
-				<button>
-					<img src={assets.user_icon} alt="user icon" />
-				</button>
-			</div> */}
 
 			{/* For phone screens */}
 			<div className="md:hidden flex items-center justify-between w-full text-white font-medium">
-				{/* Links */}
 				<div className="flex gap-1">
 					<Link
 						to="/become-educator"
@@ -73,8 +59,6 @@ const Navbar = () => {
 						My Enrollments
 					</Link>
 				</div>
-
-				{/* User Icon Button */}
 				<button className="bg-white rounded-full p-2 shadow hover:bg-blue-100 transition">
 					<img
 						src={assets.user_icon}
