cf08e0f789338c8ddc0ba5ebd20175258d0d6426
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-09 12:07:50 -0400

    hide some links when user is not signed in

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -20,32 +20,8 @@ const Navbar = () => {
 				alt="logo"
 				className=" w-21 md:w-28 lg:w-32 cursor-pointer"
 			/>
-
-			<div className="hidden md:flex items-center gap-12 text-white font-medium">
-				<Link
-					to="/become-educator"
-					className="hover:underline transition duration-200"
-				>
-					Become Educator
-				</Link>
-				<Link
-					to="/my-enrollments"
-					className="hover:underline transition duration-200"
-				>
-					My Enrollments
-				</Link>
-			</div>
-
-			<button
-				onClick={() => openSignIn()}
-				className="hidden md:inline-block bg-white text-blue-600 font-semibold px-5 py-2 rounded-full shadow hover:bg-blue-100 transition duration-300"
-			>
-				Create Account
-			</button>
-
-			{/* For phone screens */}
-			<div className="md:hidden flex items-center justify-between w-full text-white font-medium">
-				<div className="flex gap-1">
+			{user && (
+				<div className="hidden md:flex items-center gap-12 text-white font-medium">
 					<Link
 						to="/become-educator"
 						className="hover:underline transition duration-200"
@@ -59,13 +35,54 @@ const Navbar = () => {
 						My Enrollments
 					</Link>
 				</div>
-				<button className="bg-white rounded-full p-2 shadow hover:bg-blue-100 transition">
-					<img
-						src={assets.user_icon}
-						alt="user icon"
-						className="w-6 h-6"
-					/>
+			)}
+			{user ? (
+				<div className="hidden md:inline-block">
+					<UserButton />
+				</div>
+			) : (
+				<button
+					onClick={() => openSignIn()}
+					className="hidden md:inline-block bg-white text-blue-600 font-semibold px-5 py-2 rounded-full shadow hover:bg-blue-100 transition duration-300"
+				>
+					Create Account
 				</button>
+			)}
+
+			{/* For phone screens */}
+			<div className="md:hidden flex items-center justify-between w-full text-white font-medium">
+				<div className="flex items-center gap-1 sm:gap-2 max-sm:text-sx">
+					{user && (
+						<div className="flex gap-1">
+							<Link
+								to="/become-educator"
+								className="hover:underline transition duration-200"
+							>
+								Become Educator
+							</Link>
+							<Link
+								to="/my-enrollments"
+								className="hover:underline transition duration-200"
+							>
+								My Enrollments
+							</Link>
+						</div>
+					)}
+				</div>
+				{user ? (
+					<UserButton />
+				) : (
+					<button
+						onClick={() => openSignIn()}
+						className="bg-white rounded-full p-2 shadow hover:bg-blue-100 transition"
+					>
+						<img
+							src={assets.user_icon}
+							alt="user icon"
+							className="w-6 h-6"
+						/>
+					</button>
+				)}
 			</div>
 		</div>
 	);
